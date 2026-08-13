8f1128d2f715898618a19f2eb7402d7f6994358a
luanaveigasilva-prog <luana.veiga.silva@escola.pr.gov.br>
2026-08-13 19:08:46 +0000

    ok

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
+document.addEventListener("DOMContentLoaded",)())=> {
+    const likebtn = document.querySelector)(".left-actions .action-btn:first-child");
+        if (! likebtn) return;
+        const likesSvg = likeben.querySelector("svg");
 
+//localiza o contador
 
-
+let textNode = Array
 
 
 
